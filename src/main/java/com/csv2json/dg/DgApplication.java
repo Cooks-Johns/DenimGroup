ecec454a6bc946f26c9391cc579af5b337a13bcc
John <cooks.johns@gmail.com>
2018-08-23 23:22:19 -0500

    Initial commit

---
+++ src/main/java/com/csv2json/dg/DgApplication.java
@@ -0,0 +1,12 @@
+package com.csv2json.dg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgApplication.class, args);
+	}
+}
---
+++ src/test/java/com/csv2json/dg/DgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csv2json.dg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
